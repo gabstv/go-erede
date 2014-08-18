bdd75dd09f239d7d8499c1613904e2ba9dbbf1ca
Gabriel Ochsenhofer <gabs@gabs.tv>
2014-08-18 10:51:39 -0300

    check boleto status func

--- core.go
+++ core.go
@@ -292,6 +292,42 @@ func ConfirmDebitTxn(gatewayRef, debitPaRes string, reqccbuf, respccbuf io.Write
 	return tr, err
 }
 
+func ConsultBoleto(gatewayRef string, reqccbuf, respccbuf io.Writer) (*TransactionResponse, error) {
+	tpl := new(bytes.Buffer)
+	tpl.WriteString(`<Request version="2">`)
+	tpl.WriteString("<Authentication><AcquirerCode><rdcd_pv>")
+	tpl.WriteString(User)
+	tpl.WriteString("</rdcd_pv></AcquirerCode><password>")
+	tpl.WriteString(Password)
+	tpl.WriteString("</password></Authentication>")
+	tpl.WriteString("<Transaction><HistoricTxn><reference>")
+	tpl.WriteString(gatewayRef)
+	tpl.WriteString("</reference>")
+	tpl.WriteString("<method>query</method></HistoricTxn></Transaction></Request>")
+	if Verbose {
+		log.Println("Sending ConsultBoleto: ", tpl.String())
+	}
+	if reqccbuf != nil {
+		reqccbuf.Write(tpl.Bytes())
+	}
+	resp, err := http.Post(URL(), "application/xml", tpl)
+	if err != nil {
+		return nil, err
+	}
+	tpl.Truncate(0)
+	defer resp.Body.Close()
+	io.Copy(tpl, resp.Body)
+	if Verbose {
+		log.Println("XML RESPONSE", tpl.String())
+	}
+	if respccbuf != nil {
+		respccbuf.Write(tpl.Bytes())
+	}
+	tr := &TransactionResponse{}
+	err = xml.Unmarshal(tpl.Bytes(), tr)
+	return tr, err
+}
+
 func (t *Transaction) getXMLBoleto() *bytes.Buffer {
 	vals := make(map[string]interface{}, 0)
 	vals["User"] = User
